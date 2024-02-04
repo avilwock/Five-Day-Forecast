589693a3a7017b7f54059f88fdc603e72612ca43
avilwock <151412286+avilwock@users.noreply.github.com>
2024-02-03 17:24:28 -0800

    populated six days of data, fixed css formatting, and fixed div endings in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
         var apiKey = "45321ce296030dae198763830cd900c8";
         var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+        var currentDayUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" +apiKey;
+        
+        fetch (currentDayUrl)
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error("network response was not okay. Status Code: " +response.status);   
+                }
+            return response.json();
+            })
+            .then(function (data) {
+                console.log (data);
+                var i = 1;
+                var currentWeather = data;
+                var tempKelvin = currentWeather.main.temp
+                iconCode = currentWeather.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/w/" +iconCode+ ".png";
+                                    
+                var currentDay = dayjs(currentWeather.dt*1000).format('MM/DD/YYYY dddd');
+
+                document.getElementById("icon-" + i).src = iconUrl;
+                document.getElementById("current-city").textContent = data.name;
+                document.getElementById("current-temp-" + i).textContent = "Temperature: " + ((tempKelvin-273.15)*9/5+32).toFixed(0) + " °F";
+                document.getElementById("current-humidity-" + i).textContent = "Humidity: " + currentWeather.main.humidity + "%";
+                document.getElementById("current-wind-" + i).textContent = "Wind Speed: " + currentWeather.wind.speed + " m/s";
+                
+                document.getElementById("day-" + i).textContent = "Current Day: " + currentDay;
+                console.log(data);
+            })
 
         fetch(requestUrl)
             .then(function(response) {
@@ -30,25 +58,34 @@ document.addEventListener("DOMContentLoaded", function () {
             return response.json();
             })
             .then(function (data) {
-                var currentWeather = data.list[0];
-                var tempKelvin = currentWeather.main.temp
-                iconCode = currentWeather.weather[0].icon;
-                var iconUrl = "http://openweathermap.org/img/w/" +iconCode+ ".png";
-                
-                var currentDay = dayjs().format('MM/DD/YYYY');
+                for ( var i = 2; i < 7; i++) {
+                    var num = (i-2) *8;
 
-                document.getElementById("icon").src = iconUrl;
-                document.getElementById("current-city").textContent = data.city.name;
-                document.getElementById("current-temp").textContent = "Temperature: " + ((tempKelvin-273.15)*9/5+32).toFixed(0) + " °F";
-                document.getElementById("current-humidity").textContent = "Humidity: " + currentWeather.main.humidity + "%";
-                document.getElementById("current-wind").textContent = "Wind Speed: " + currentWeather.wind.speed + " m/s";
-                
-                document.getElementById("current-day").textContent = "Current Day: " + currentDay;
-                console.log(data);
+                    var currentWeather = data.list[num];
+                    var tempKelvin = currentWeather.main.temp
+                    iconCode = currentWeather.weather[0].icon;
+                    var iconUrl = "http://openweathermap.org/img/w/" +iconCode+ ".png";
+                                        
+                    var currentDay = dayjs(currentWeather.dt*1000).format('MM/DD/YYYY dddd');
 
-                localStorage.setItem('cityName', cityName);
+                    document.getElementById("icon-" + i).src = iconUrl;
+                    document.getElementById("current-city").textContent = data.city.name;
+                    document.getElementById("current-temp-" + i).textContent = "Temperature: " + ((tempKelvin-273.15)*9/5+32).toFixed(0) + " °F";
+                    document.getElementById("current-humidity-" + i).textContent = "Humidity: " + currentWeather.main.humidity + "%";
+                    document.getElementById("current-wind-" + i).textContent = "Wind Speed: " + currentWeather.wind.speed + " m/s";
+                    
+                    document.getElementById("day-" + i).textContent = "Current Day: " + currentDay;
+                    console.log(data);
+                }
+                // var locationArray = JSON.parse(localStorage.getItem("cityList")) || [];
+                // locationArray.push (cityName)
+                // localStorage.setItem('cityList', JSON.stringify(locationArray));
+
                 // Update the text content of the <p> element
-                document.querySelector('#search-storage p').textContent = cityName;
+                
+                // for (var i = 1; i < 6; i++) {
+                //     document.querySelector('#search-storage' + i).textContent = locationArray[i];
+                // }
                 });
             })
             // .catch(function(error) {
